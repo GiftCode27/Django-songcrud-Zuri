7d0d544a0d240f1c0665916c421ba5cbe5f6c488
GiftCode27 <okagift@gmail.com>
2022-10-28 17:44:11 +0100

    created lyric,artiste and song relationship

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import artiste, song, lyric
 # Register your models here.
+
+admin.site.register(artiste)
+admin.site.register(song)
+admin.site.register(lyric)
